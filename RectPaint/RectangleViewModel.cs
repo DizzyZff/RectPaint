10004b8d75f212c9ffec4b7bc73348400eff2f7f
Desmond Zhu <hz1778@nyu.edu>
2023-03-04 23:01:44 -0500

    Update MainWindowViewModel.cs

---
+++ RectPaint/RectangleViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel;
+using System.Windows.Media;
+
+namespace RectPaint
+{
+    public class RectangleViewModel : INotifyPropertyChanged
+    {
+        
+        private SolidColorBrush _fill = new SolidColorBrush(Colors.Red);
+        public SolidColorBrush Fill
+        {
+            get => _fill;
+            set
+            {
+                _fill = value;
+                OnPropertyChanged(nameof(Fill));
+            }
+        }
+        
+        private SolidColorBrush _stroke = new SolidColorBrush(Colors.Black);
+        public SolidColorBrush Stroke
+        {
+            get => _stroke;
+            set
+            {
+                _stroke = value;
+                OnPropertyChanged(nameof(Stroke));
+            }
+        }
+        
+        private double _strokeThickness = 1;
+        public double StrokeThickness
+        {
+            get => _strokeThickness;
+            set
+            {
+                _strokeThickness = value;
+                OnPropertyChanged(nameof(StrokeThickness));
+            }
+        }
+        
+        private double _x = 0;
+        public double X
+        {
+            get => _x;
+            set
+            {
+                _x = value;
+                OnPropertyChanged(nameof(X));
+            }
+        }
+        
+        private double _y = 0;
+        public double Y
+        {
+            get => _y;
+            set
+            {
+                _y = value;
+                OnPropertyChanged(nameof(Y));
+            }
+        }
+        
+        private double _width = 0;
+        public double Width
+        {
+            get => _width;
+            set
+            {
+                _width = value;
+                OnPropertyChanged(nameof(Width));
+            }
+        }
+        
+        private double _height = 0;
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                _height = value;
+                OnPropertyChanged(nameof(Height));
+            }
+        }
+        
+        private bool isSelected = false;
+        public bool IsSelected
+        {
+            get => isSelected;
+            set
+            {
+                isSelected = value;
+                OnPropertyChanged(nameof(IsSelected));
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        
+    }
+}
