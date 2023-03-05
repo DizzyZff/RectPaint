6bf67995ebbc5584d3c27cf0fbefb8c8201e5fa7
Desmond Zhu <hz1778@nyu.edu>
2023-03-04 21:27:57 -0500

    Finish UI design

---
+++ RectPaint/ImageCanvas.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace RectPaint
+{
+    public partial class ImageCanvas : UserControl
+    {
+        public ImageCanvas()
+        {
+            InitializeComponent();
+        }
+    }
+}
