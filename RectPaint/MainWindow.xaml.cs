e1503c67fe521fcb3e76c3ce71d6c85cea534aee
Desmond Zhu <hz1778@nyu.edu>
2023-03-04 22:08:42 -0500

    Update ImageCanvas.xaml

--- RectPaint/MainWindow.xaml.cs
+++ RectPaint/MainWindow.xaml.cs
@@ -10,7 +10,7 @@ namespace RectPaint
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainWindowViewModel();
+            //DataContext = new MainWindowViewModel();
         }
     }
 }
